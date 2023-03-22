71e6d99814ee427484c239fcba1b29348d96a975
fifidonnelly <126438786+fifidonnelly@users.noreply.github.com>
2023-03-22 12:57:45 +0800

    added functionality to button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// Empty script file to start with
+//Step 1 Find the element 
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("task-input");
+var todoListContainer= document.getElementById("todo-list");
+
+//Step 2 Write the behaviour 
+function onAddTaskClicked(event) {
+    var taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    todoListContainer.insertAdjacentElement('afterbegin', taskName)
+}
+
+//Step 3 Link to event handler 
+addTaskButton.addEventListener('click', onAddTaskClicked); 
