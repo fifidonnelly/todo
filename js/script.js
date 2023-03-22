69a5e168e601f37b6af03f612aba7a2a3e772493
fifidonnelly <126438786+fifidonnelly@users.noreply.github.com>
2023-03-22 13:32:16 +0800

    added functionality

--- js/script.js
+++ js/script.js
@@ -13,5 +13,19 @@ function onAddTaskClicked(event) {
     todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
+function onTodoListContainerClicked(event){
+while(!targetElement.classList.contains("task")) {
+    targetElement = targetElement.parentElement; 
+}
+var checkbox = targetElement.querySelector(".checkbox");
+if(checkbox.checked){
+    targetElement.classList.add("completed")
+}
+else{
+    targetElement.classList.remove("completed")
+}
+}
+
 //Step 3 Link to event handler 
 addTaskButton.addEventListener('click', onAddTaskClicked); 
+todoListContainer.addEventListener('click', onTodoListContainerClicked);
